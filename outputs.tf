8c9ff238cedebaabe4def8a199f1a2f796ed0525
Kowsalya-7 <kowsalyaganta@gmail.com>
2025-08-20 19:16:39 +0530

    output file added

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.my_ec2.public_ip
 }
+
+output "private_ip" {
+  description = "The private IP address of the EC2 instance"
+  value       = aws_instance.my_ec2.private_ip
+}
